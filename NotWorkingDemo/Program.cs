08386666553e0192ccb6bb9cd8bb5b6e0b3e4e1b
SydneyOwl <mrowl@mrowl.xyz>
2026-01-21 09:57:46 +0800

    Reproduce problems

---
+++ NotWorkingDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Antelcat.I18N.Avalonia;
+using LangKeys = NotWorkingDemo.Resources.LangKeys;
+
+internal sealed class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        LangKeys.LanguageProvider.Initialize();
+
+        I18NExtension.Culture = new CultureInfo("zh-CN");
+        Console.WriteLine(I18NExtension.Translate("accept"));
+        I18NExtension.Culture = new CultureInfo("en-US");
+        Console.WriteLine(I18NExtension.Translate("accept"));
+    }
+}
---
+++ NotWorkingDemo/Resources/LangKeys.cs
@@ -0,0 +1,11 @@
+﻿using Antelcat.I18N.Attributes;
+
+namespace NotWorkingDemo.Resources;
+
+/// <summary>
+///     I18N conf class.
+/// </summary>
+[ResourceKeysOf(typeof(Language.Language))]
+public static partial class LangKeys
+{
+}
